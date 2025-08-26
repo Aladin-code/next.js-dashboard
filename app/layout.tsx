d370a6df65c214fad7893eeb7290dfb2c6280ba3
Aladin-code <cachoaladin192@gmail.com>
2025-08-26 14:13:26 +0800

    fix: GA4 errors and issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,34 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import Script from 'next/script';
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics (GA4) */}
-        {/* Google tag (gtag.js) */}
-<Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-09J7X11MV1"
-  strategy="afterInteractive"
-/>
-<Script id="ga-debug" strategy="afterInteractive">
+        {/* GA4 script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-09J7X11MV1"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-09J7X11MV1', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
+        {/* Debugger: logs all GA4 events */}
+        <Script id="ga-debug" strategy="afterInteractive">
+          {`
             const originalPush = window.dataLayer.push;
             window.dataLayer.push = function() {
               console.log("GA4 event fired:", arguments);
@@ -27,7 +37,9 @@ export default function RootLayout({
           `}
         </Script>
       </head>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
